7527bd49ca58750875939825a5e86790b3148edb
bnoden <brandroid369@gmail.com>
2017-06-12 20:00:58 -0500

    .

---
+++ 2_1.py
@@ -0,0 +1,14 @@
+# 1.Personal information
+# Write a program that displays the following information:
+# Your name
+# Your address, with city, state, and ZIP
+# Your telephone
+# number
+# Your college major
+
+print("""Cranston
+12345 Asp Net
+Computer City, TX 75319
+555-777-9999
+Food Science
+""")
